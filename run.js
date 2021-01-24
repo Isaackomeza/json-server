89f34c80f95489685df981e30124ec93128e051e
Isaac <isaackomeza@gmail.com>
2021-01-24 16:47:14 +0200

    fixx

--- run.js
+++ run.js
@@ -1,1 +1,6 @@
 // json-server --watch db.json -p 3001 -d 2000
+
+// Use the following URL to fetch this data 
+
+// export const baseUrl = 'https://my-json-server.typicode.com/Isaackomeza/json-server/';
+// export const baseUrl = 'http://localhost:3001/';
